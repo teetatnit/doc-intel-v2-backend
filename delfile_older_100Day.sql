360d1949c6aa5c01839040b36f2710da828aed5f
teetatni <teetatni@scg.com>
2024-07-15 16:39:27 +0700

    .

--- delfile_older_100Day.sql
+++
@@ -1,3 +0,0 @@
-  delete from [SKIC_EXPO].[dbo].[files] where [create_date] <= DATEADD(day,-100,GETDATE());
-  delete from [SKIC_EXPO].[dbo].[po] where [create_date] <= DATEADD(day,-100,GETDATE());
-  delete from [SKIC_EXPO].[dbo].[po_items] where [create_date] <= DATEADD(day,-100,GETDATE());
